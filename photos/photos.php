e8e8f39ce7c15f66de128077ada1d02adad7f931
Peer Study <arunava.ganguly.07@gmail.com>
2020-12-12 19:11:26 -0800

    Updated

---
+++ photos/photos.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+
+	<meta charset="utf-8" /> 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-9586850-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-9586850-3');
+</script>
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="referrer" content="same-origin" />
+		<meta name="robots" content="index, follow" />
+		
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:title" content="Photos | Nilanjan Goswami, Ph.D.">
+	<meta name="twitter:image" content="http://www.nilanjan.info/resources/NilG.png">
+	<meta name="twitter:url" content="http://www.nilanjan.info/photos/photos.php">
+	<meta property="og:type" content="website">
+	<meta property="og:site_name" content="Nilanjan Goswami, Ph.D.">
+	<meta property="og:title" content="Photos | Nilanjan Goswami, Ph.D.">
+	<meta property="og:image" content="http://www.nilanjan.info/resources/NilG.png">
+	<meta property="og:url" content="http://www.nilanjan.info/photos/photos.php"> 
+
+	<!-- Meta tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+	<title>Photos | Nilanjan Goswami, Ph.D.</title>
+	<link rel="stylesheet" type="text/css" media="all" href="../rw_common/themes/Artful/consolidated-10.css?rwcache=629497819" />
+		
+	    		<link rel='stylesheet' type='text/css' media='all' href='../rw_common/plugins/stacks/stacks.css?rwcache=629497819' />
+		<link rel='stylesheet' type='text/css' media='all' href='files/stacks_page_page10.css?rwcache=629497819' />
+        <script type='text/javascript' charset='utf-8' src='../rw_common/plugins/stacks/jquery-2.2.4.min.js?rwcache=629497819'></script>
+        
+        
+        
+		
+		<script type='text/javascript' charset='utf-8' src='files/stacks_page_page10.js?rwcache=629497819'></script>
+        <meta name="formatter" content="Stacks v3.6.9 (4242)" >
+		<meta class="stacks3 stack version" id="de.futural.gallerystack3" name="Gallery 3" content="3.11.1">
+		<meta class="stacks3 stack version" id="de.futural.gallerystack3.lightbox.lightgallery" name="Lightbox: lightGallery" content="3.0.0">
+		<meta class="stacks3 stack version" id="de.futural.gallerystack3.grid.instagram" name="Grid: Instagram" content="3.0.0">
+		<meta class="stacks3 stack version" id="de.futural.gallerystack3.imagepro" name="Image + Thumb Pro" content="3.0.0">
+		
+
+</head>
+
+<!-- This page was created with RapidWeaver from Realmac Software. http://www.realmacsoftware.com -->
+
+<body>
+	<nav class="navbar navbar-expand-lg">
+		<div class="container">
+            <a class="navbar-brand" href="http://www.nilanjan.info/">
+                <div class="logo"></div>
+                <span class="navbar-title">Photography</span>
+            </a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+			 aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ml-auto"><li class="nav-item"><a href="../" rel="" class="nav-link">Home</a></li><li class="nav-item"><a href="../collab/collab.html" rel="" class="nav-link">Collaboration</a></li><li class="nav-item"><a href="../pub/publication.html" rel="" class="nav-link">Publication</a></li><li class="nav-item"><a href="../service/service.html" rel="" class="nav-link">Service</a></li><li class="nav-item active"><a href="photos.php" rel="" class="nav-link">Photos</a></li><li class="nav-item"><a href="../contact/contact.html" rel="" class="nav-link">Contact</a></li></ul>
+			</div>
+		</div>
+	</nav>
+	<div class="hero pb-5">
+		<div class="container">
+			<div class="row align-items-center justify-content-center">
+				<div class="col-sm-6 align-self-center">
+					<h1 class="hero-title mb-4">
+						Photography
+					</h1>
+					<p class="lead hero-slogan">
+						
+					</p>
+				</div>
+				<div class="col-sm-6">
+					<img src="../rw_common/images/NilG.png" width="100" alt="" class="banner_image">
+				</div>
+			</div>
+		</div>
+    </div>
+
+    <hr class="my-5">
+
+	<div class="container pt-5">
+        <div class="row intro justify-content-between">
+            <div class="col-sm-12">
+                
+<div id='stacks_out_1_page10' class='stacks_top'><div id='stacks_in_1_page10' class=''><div id='stacks_out_2_page10' class='stacks_out'><div id='stacks_in_2_page10' class='stacks_in text_stack'><span style="font-size:18px; color:#274169;">Follow me on Instagram </span><span style="font-size:18px; color:#274169;"><a href="https://www.instagram.com/nilg.photography/" target="_blank">@nilg.photography</a></span></div></div><div id='stacks_out_4_page10' class='stacks_out'><div id='stacks_in_4_page10' class='stacks_in com_yourhead_stacks_two_columns_stack'><div class='s3_row'>
+	<div class='s3_column s3_column_left'><div class='slice empty out'><div class='slice empty in'></div></div></div>
+	<div class='s3_column s3_column_right'><div id='stacks_out_6_page10' class='stacks_out'><div id='stacks_in_6_page10' class='stacks_in text_stack'><br /><br /></div></div></div>
+</div></div></div><div id='stacks_out_9_page10' class='stacks_out'><div id='stacks_in_9_page10' class='stacks_in de_futural_gallerystack3_stack'><div class="stacks_in_9_page10_edit_mode_wrapper stacks_in_9_page10_grid_slider_wrapper">
+<div id='stacks_out_10_page10' class='stacks_out'><div id='stacks_in_10_page10' class='stacks_in de_futural_gallerystack3_grid_instagram_stack'></div></div>
+</div>
+<div class="stacks_in_9_page10_edit_mode_wrapper stacks_in_9_page10_lightbox_wrapper">
+<div id='stacks_out_26_page10' class='stacks_out'><div id='stacks_in_26_page10' class='stacks_in de_futural_gallerystack3_lightbox_lightgallery_stack'></div></div>
+</div>
+<div class="stacks_in_9_page10_edit_mode_wrapper stacks_in_9_page10_photo_wrapper">
+<div id='stacks_out_14_page10' class='stacks_out'><div id='stacks_in_14_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_14.jpg" data-sm-src="files/sq_14.jpg" data-lg-alt="Yosemite Valley" data-sm-alt="Yosemite Valley"/></div>
+
+
+</div></div><div id='stacks_out_15_page10' class='stacks_out'><div id='stacks_in_15_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_15.jpg" data-sm-src="files/sq_15.jpg" data-lg-alt="Eastern Sierra Fall Color" data-sm-alt="Eastern Sierra Fall Color"/></div>
+
+
+</div></div><div id='stacks_out_16_page10' class='stacks_out'><div id='stacks_in_16_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_16.jpg" data-sm-src="files/sq_16.jpg" data-lg-alt="Yosemite Tunnel View" data-sm-alt="Yosemite Tunnel View"/></div>
+
+
+</div></div><div id='stacks_out_17_page10' class='stacks_out'><div id='stacks_in_17_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_17.jpg" data-sm-src="files/sq_17.jpg" data-lg-alt="Fisherman" data-sm-alt="Fisherman"/></div>
+
+
+</div></div><div id='stacks_out_18_page10' class='stacks_out'><div id='stacks_in_18_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_18.jpg" data-sm-src="files/sq_18.jpg" data-lg-alt="Lonely Traveller" data-sm-alt="Lonely Traveller"/></div>
+
+
+</div></div><div id='stacks_out_19_page10' class='stacks_out'><div id='stacks_in_19_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_19.jpg" data-sm-src="files/sq_19.jpg" data-lg-alt="Magnific Mountain" data-sm-alt="Magnific Mountain"/></div>
+
+
+</div></div><div id='stacks_out_20_page10' class='stacks_out'><div id='stacks_in_20_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_20.jpg" data-sm-src="files/sq_20.jpg" data-lg-alt="The Bridge" data-sm-alt="The Bridge"/></div>
+
+
+</div></div><div id='stacks_out_21_page10' class='stacks_out'><div id='stacks_in_21_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_21.jpg" data-sm-src="files/sq_21.jpg" data-lg-alt="Island Life" data-sm-alt="Island Life"/></div>
+
+
+</div></div><div id='stacks_out_22_page10' class='stacks_out'><div id='stacks_in_22_page10' class='stacks_in de_futural_gallerystack3_imagepro_stack'>
+
+
+
+<div data-lg-src="files/lg_22.jpg" data-sm-src="files/sq_22.jpg" data-lg-alt="Mount Hood" data-sm-alt="Mount Hood"/></div>
+
+
+</div></div>
+</div></div></div></div></div>
+
+            </div>
+            <div class="sidebar col-sm-6 order-md-8">
+                <h2 class="sidebar-title">
+                    
+                </h2>
+
+                 
+            </div>
+        </div>
+	</div>
+
+	<div class="footer">
+		<div class="container">
+			<div class="row justify-content-start">
+				<div class="col">
+					<div class="footer-content">
+						
+					</div>
+
+					<ul class="navbar-nav ml-auto"><li class="nav-item"><a href="../" rel="" class="nav-link">Home</a></li><li class="nav-item"><a href="../collab/collab.html" rel="" class="nav-link">Collaboration</a></li><li class="nav-item"><a href="../pub/publication.html" rel="" class="nav-link">Publication</a></li><li class="nav-item"><a href="../service/service.html" rel="" class="nav-link">Service</a></li><li class="nav-item active"><a href="photos.php" rel="" class="nav-link">Photos</a></li><li class="nav-item"><a href="../contact/contact.html" rel="" class="nav-link">Contact</a></li></ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="../rw_common/themes/Artful/js/main.js?rwcache=629497819"></script>
+<div id="rapidweaver_privacy_message">
+    <p><span style="font:13px .AppleSystemUIFont; color:#000000;">Privacy: This website does not collect any personal information such as IP address, location, language or any other user details. </span></p>
+    <button id="rapidweaver_privacy_message_dismiss_button">Dismiss</button>
+</div>
+
+<script src="../rw_common/assets/message.js?rwcache=629497819"></script>
+<script id="godaddy-security-s" src="https://cdn.sucuri.net/badge/badge.js" data-s="2049" data-i="149be1c38b868cb487e9d2ba46a5ceacf68525b157" data-p="r" data-c="l" data-t="g"></script></body>
+
+</html>
